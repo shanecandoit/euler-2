4e3707bc972f9abfc0057327070ee071cf4202c4
outOfBounds <shanecandoit@gmail.com>
2011-12-19 11:14:03 -0600

    logic in for loop results

--- main.cpp
+++ main.cpp
@@ -4,21 +4,22 @@
 int main()
 {
     unsigned long sum=0;
-    unsigned long a=0;
-    unsigned long b=1;
-    for( long num=1; num<4000000; ++num)
+    unsigned long a;
+    unsigned long b;
+    unsigned long temp;
+    for( a=0,b=1; 
+        b<4000000; 
+        temp=a+b, a=b, b=temp
+    )
     {
-        if(num%2==0)
+        if(b%2==0)
         {
             // even
-            sum+=num;
+            sum+=b;
         }
-        unsigned long temp=a+b;
-        a=b;
-        b=temp;
     }
     printf("sum: %d \n",sum);
-    //sum: 
+    //sum: 4613732
     
     return 0;
 }
